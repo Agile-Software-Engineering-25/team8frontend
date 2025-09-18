4e42945788a30bf4b2eaad68beaf4a94957a41b2
survivalizeed <gerbdm@gmail.com>
2025-09-18 14:51:23 +0200

    clean up and demo login for testing

--- src/@types/agile-shared-components.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@agile-software/shared-components' {
-  export const createCustomTheme: (config: Record<string, unknown>) =>
-    | { $$joy: Record<string, unknown> }
-    | {
-        cssVarPrefix?: string;
-        colorSchemes: Record<string, Record<string, unknown>>;
-      };
-}
--- src/config.ts
+++
@@ -1,3 +0,0 @@
-const BACKEND_BASE_URL = 'https://api.brightsky.dev';
-
-export { BACKEND_BASE_URL };
