2a38cda453e58d6f94a9113998359377ec1acb9b
survivalizeed <gerbdm@gmail.com>
2025-11-04 16:30:24 +0100

    added functional sites with mockup apis to add users to a role

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Startseite } from './Startseite.tsx';
 import { EditRolePage } from './role/RolleEditieren.tsx';
 import { UserBaseRoleList } from './base-role/UserBaseRoleList.tsx';
+import { RoleUserList } from './role-users/RoleUserList.tsx';
 
 import './index.css';
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Route path="/" element={<Startseite />} />
       <Route path="/role/:roleId" element={<EditRolePage />} />
       <Route path="/base-role/:baseRoleId" element={<UserBaseRoleList />} />
+       <Route path="/role-users/:roleId" element={<RoleUserList />} />
     </Routes>
   </BrowserRouter>
 );
