445ca4e3c8c815561c63f12e35df296410aaea36
Phong Geerken <gagasepp@yahoo.de>
2025-11-14 15:12:15 +0100

    kill the linter

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
+export{};
 
 interface ImportMetaEnv {
   readonly VITE_BACKEND_BASE_URL?: string;
 }
 
-interface ImportMeta {readonly env: ImportMetaEnv;};
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
