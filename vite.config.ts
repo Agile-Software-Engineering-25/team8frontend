25eb283dbc63cdb23eb1ee4ee3cbd86b49eb0c18
Phong Geerken <gagasepp@yahoo.de>
2025-11-14 10:30:55 +0100

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vitePluginSingleSpa from 'vite-plugin-single-spa';
 import vitePluginReactHMR from 'vite-plugin-react-single-spa-hmr';
 
 const PORT = parseInt(process.env.PORT ?? '5173');
-const BASE_URL_DEPLOYMENT = `http://localhost:${PORT}/`;
+//const BASE_URL_DEPLOYMENT = `http://localhost:${PORT}/`;
 
 const ENTRY_POINT = 'src/singleSpa.tsx';
 
